b8e3edb3d10d650dec117048562ab7d073d031e9
Webdevhead <webdevhead@pop-os.localdomain>
2023-08-06 15:54:46 -0400

    Enableling the post button when user types

--- 1_CREATING_POST_FORM.md
+++ 1_CREATING_POST_FORM.md
@@ -2,7 +2,6 @@
 
 - / > mkdir mixins > cd mixins > touch mixins.pug
 - mixins.pug > create mixins
-- main-layout.pug > include mixins
 - home.pug > add mixins
 
 # Add defualt profile picture
--- public/js/common.js
+++ public/js/common.js
@@ -1,10 +1,24 @@
 // common.js : where all the shared scripts between the site will be (  reusable shared code )
 
 const postTextArea = document.querySelector('#postTextArea');
-postTextArea.onkeyup = function (onKeyBoardEvent) {
-  // event is the KeyboardEvent object
-  // target is #postTextArea
-  // value is user input
-  const textBox = onKeyBoardEvent.target.value.trim();
-  console.log(textBox);
+const submitButton = document.querySelector('#submitPostButton');
+
+// event is the KeyboardEvent object
+// target is textarea#postTextArea
+// value is user input
+
+postTextArea.onkeyup = function (onKeyupEvent) {
+  // get postTextArea user inputed value
+  const value = onKeyupEvent.target.value.trim();
+  //  if there is no button
+  if (submitButton.length == 0) alert('No submit button');
+  if (value === '') {
+    // if user post nothing set submit button disabled to true
+    submitButton.setAttribute('disabled', true);
+    submitButton.style.backgroundColor = '#9bd1f9';
+    return;
+  }
+  // if user post something
+  submitButton.setAttribute('disabled', false);
+  submitButton.style.backgroundColor = '#1fa2f1';
 };
